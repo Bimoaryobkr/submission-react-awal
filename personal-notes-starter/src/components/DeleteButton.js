8dece146c1ca763880ecc33c8d545e9fc5ee6d54
Bimo Aryo Bramantyo KR <101321869+Bimoaryobkr@users.noreply.github.com>
2022-12-05 22:27:12 +0700

    Delete personal-notes-starter directory

--- personal-notes-starter/src/components/DeleteButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function DeleteButton({ id, onDelete }) {
-    return <button className='item_delete' onClick={() => onDelete(id)}>X</button>
-}
-
-export default DeleteButton;
